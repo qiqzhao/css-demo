30f802cff3d6150c425b7f7aeaff2fc44ec8620b
qiqzhao <bme_ritter@foxmail.com>
2024-07-30 14:37:29 +0800

    feat: valid img

---
+++ valide-img/captcha.js
@@ -0,0 +1,34 @@
+const captcha = document.querySelector("#captcha");
+const handle = document.querySelector("#handle");
+const button = document.querySelector("#handle span");
+const body = document.querySelector("body");
+
+const oLeft = handle.getBoundingClientRect().left;
+const buttonWidth = handle.getBoundingClientRect().width;
+
+let flag = false;
+
+button.addEventListener("mousedown", (e) => {
+  flag = true;
+});
+
+window.addEventListener("mousemove", (e) => {
+  if (flag) {
+    captcha.style.setProperty(
+      "--moved",
+      `${e.clientX - oLeft - buttonWidth / 2}px`
+    );
+  }
+});
+
+window.addEventListener("mouseup", (e) => {
+  if (flag) {
+    const dis = e.clientX - oLeft;
+    if (dis >= dis - 2 && dis <= dis + 2) {
+      captcha.classList.add("passed");
+    } else {
+      captcha.style.setProperty("--moved", "0px");
+    }
+    flag = false;
+  }
+});
